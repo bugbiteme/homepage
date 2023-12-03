28262f6d789665a42d5fdc05532d8dad871da77c
Leon Levy <leon.peon@gmail.com>
2023-12-03 14:38:28 -0800

    more certs to brag about

--- startcontainer.sh
+++ startcontainer.sh
@@ -2,4 +2,4 @@
 
 # launch the container and mount the access log directory
 # to local directory
-podman run -d -p 80:80 --name pi_nginx -v ~/var/log/nginx:/var/log/nginx pi_nginx
+podman run -d -p 80:80 --name pi_nginx -v /Users/leonlevy/var/log:/var/log/nginx localhost/mi-nginx
